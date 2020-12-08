c2917b9bc9a77983b53deff708bf24a86a8851fa
JohnatanCarlos <johnatan.carlos007@hotmail.com>
2020-12-07 22:23:05 -0300

    primeiro comite projeto curso alura vue

---
+++ alurapic/src/components/shared/imagem-responsiva/imagemResponsiva.vue
@@ -0,0 +1,15 @@
+<template>
+    <img class="img-resposiva" :src="url" :alt="titulo">
+</template>
+
+<script>
+export default {
+    props: ['url', 'titulo']
+}
+</script>
+
+<style scoped>
+    .img-resposiva{
+        width: 100%;
+    }
+</style>
